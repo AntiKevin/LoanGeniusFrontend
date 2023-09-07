fa6ccd64e9f3569f364ab3d36f6dbcc21094e127
antikevin <Kevin_3467@yahoo.com>
2023-09-07 16:33:59 -0300

    build: :rocket: changing api url to production

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://127.0.0.1:8080'
+    baseURL: 'https://api-loangenius.up.railway.app'
   },
 
   auth: {
